acb21b1b781fff32dd68a6b20af7e231ef954eec
Bubio <78299054+bubio@users.noreply.github.com>
2023-04-04 19:10:09 +0900

    マウスの戻るボタンでメニューを戻れるようにした (#10)

--- UI/menulist.cpp
+++ UI/menulist.cpp
@@ -1041,14 +1041,13 @@ void MenuList::OnMouseButtonDown(SDL_Event *e)
 		return;
 	}
 
-	// right button ?
-	if ((e->button.button == SDL_BUTTON_RIGHT) && (e->button.state == SDL_PRESSED)) {
-		// right button
+	// left button?
+	if (e->button.button != SDL_BUTTON_LEFT) {
 		return;
 	}
 
 	// left button ?
-	if ((e->button.button == SDL_BUTTON_LEFT) && (e->button.state == SDL_PRESSED)) {
+	if (e->button.state == SDL_PRESSED) {
 		// left button -> save for ProcessMenu()
 		mouse_down = true;
 	}
@@ -1116,12 +1115,17 @@ void MenuList::OnMouseButtonUp(SDL_Event *e)
 	}
 
 	// right button ?
-	if ((e->button.button == SDL_BUTTON_RIGHT) && (e->button.state == SDL_RELEASED)) {
+	if (((e->button.button == SDL_BUTTON_RIGHT) && (e->button.state == SDL_RELEASED)) ||
+		((e->button.button == SDL_BUTTON_X1) && (e->button.state == SDL_RELEASED))) {
 		// right button -> back
 		menu->Command(false, MENU_BACK);
 		return;
 	}
 
+	if (e->button.button != SDL_BUTTON_LEFT) {
+		return;
+	}
+ 
 	// convert point
 	x = e->button.x;
 	y = e->button.y;
