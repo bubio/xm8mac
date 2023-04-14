2ddb7038bd4afd4d3a03689cd9329c62cbbafcb5
bubio <bubio66@gmail.com>
2023-04-15 00:52:36 +0900

    Change Linux build system to CMake

--- macOS/dist_app.sh
+++ macOS/dist_app.sh
@@ -6,8 +6,5 @@ brew bundle install
 # Remove previous artifact.
 rm -rf build
 
-# Create icon set
-# source iconcreate.sh AppIcon.png
-
 cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DMACOSX_STANDALONE_APP_BUNDLE=ON
 cmake --build build -j $(sysctl -n hw.physicalcpu)
