bb2cc2132f4eb653b160549d55f3f9a02756ec4e
Bubio <78299054+bubio@users.noreply.github.com>
2023-10-17 20:03:24 +0900

    Revert "At least to the point where it works."
    This reverts commit fea2ebec59cab3058619d5ee662620debb323d0f.

--- Builder/Android/app/jni/src/Android.mk
+++ Builder/Android/app/jni/src/Android.mk
@@ -15,8 +15,7 @@ LOCAL_C_INCLUDES := \
 	$(XM8_SRC_PATH)/ePC-8801MA \
 	$(XM8_SRC_PATH)/ePC-8801MA/vm \
 	$(XM8_SRC_PATH)/ePC-8801MA/vm/fmgen \
-	$(XM8_SRC_PATH)/ePC-8801MA/vm/pc8801 \
-	$(XM8_SRC_PATH)/Filter/xBRZ
+	$(XM8_SRC_PATH)/ePC-8801MA/vm/pc8801
 
 # Add your application source files here...
 LOCAL_SRC_FILES := \
@@ -24,13 +23,11 @@ LOCAL_SRC_FILES := \
     $(wildcard $(XM8_SRC_PATH)/ePC-8801MA/vm/*.cpp) \
     $(wildcard $(XM8_SRC_PATH)/ePC-8801MA/vm/fmgen/*.cpp) \
     $(wildcard $(XM8_SRC_PATH)/ePC-8801MA/vm/pc8801/*.cpp) \
-    $(wildcard $(XM8_SRC_PATH)/Filter/xBRZ/*.cpp) \
     $(wildcard $(XM8_SRC_PATH)/UI/*.cpp) \
     $(wildcard $(LOCAL_PATH)/*.c)
 
 LOCAL_SHARED_LIBRARIES := SDL2
-LOCAL_CFLAGS += -DSDL -D_PC8801MA -D__ANDROID__ -Wno-narrowing -std=c17
-LOCAL_CPPFLAGS += -DSDL -D_PC8801MA -D__ANDROID__ -Wno-narrowing -std=c++17
+LOCAL_CFLAGS += -DSDL -D_PC8801MA -D__ANDROID__ -Wno-narrowing
 LOCAL_CPP_FEATURES += exceptions
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
