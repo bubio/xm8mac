ed48e11679514a011f51555a9a158785438e88cd
Bubio <78299054+bubio@users.noreply.github.com>
2023-10-17 20:03:24 +0900

    Revert "Added image interpolation settings."
    This reverts commit 6fcf66145d15f4575d4ce35283e049ea91941f40.

--- Source/UI/setting.h
+++ Source/UI/setting.h
@@ -145,10 +145,6 @@ public:
 										// get render scale quality
 	void SetScaleQuality(int quality);
 										// set render scale quality
-	bool HasImageInterpolation();
-										// get image interpolation
-	void SetImageInterpolation(bool interpolation);
-										// set image interpolation
 	bool IsForceRGB565();
 										// get RGB565 mode (Android only)
 	void SetForceRGB565(bool enable);
