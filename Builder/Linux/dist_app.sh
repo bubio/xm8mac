1774f9d4eff8b4fbd6fe5cc1640b88d3a299c4b9
bubio <bubio66@gmail.com>
2025-08-11 18:59:18 +0900

    add linux x86_64  build

--- Builder/Linux/dist_app.sh
+++ Builder/Linux/dist_app.sh
@@ -5,11 +5,15 @@
 #
 
 # Debian and Ubuntu
-# sudo apt install build-essentials cmake libsdl2-dev
+# sudo apt install build-essential cmake libsdl2-dev
 
 # Fedora
-# sudo dnf groupinstall "Development Tools"
-# sudo dnf install cmake SDL2-devel
+# for dnf4
+#   sudo dnf groupinstall "Development Tools"
+#   sudo dnf install cmake SDL2-devel
+# for dnf5
+#   sudo dnf install @development-tools
+#   sudo dnf install cmake SDL2-devel
 
 pushd .
 cd ../../
