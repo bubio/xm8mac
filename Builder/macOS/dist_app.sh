d3d9c493c20f0388e188b2098076d6e7ad7bc25a
bubio <bubio66@gmail.com>
2025-08-16 00:25:08 +0900

    fix codesign

--- Builder/macOS/dist_app.sh
+++ Builder/macOS/dist_app.sh
@@ -11,6 +11,7 @@ rm -rf build
 
 # cmake -G Xcode -S . -B build -DCMAKE_BUILD_TYPE=Release -DMACOSX_STANDALONE_APP_BUNDLE=ON
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DMACOSX_STANDALONE_APP_BUNDLE=ON
+codesign --sign - --timestamp=none build/xm8.app
 cmake --build build -j $(sysctl -n hw.physicalcpu) --target package
 
 popd .
