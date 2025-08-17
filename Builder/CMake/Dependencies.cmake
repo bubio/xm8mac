0af08eccf10058d8e0b1fcac9b7edd23605b2cd3
bubio <bubio66@gmail.com>
2025-08-17 15:58:47 +0900

    Organize cmake

--- Builder/CMake/Dependencies.cmake
+++ Builder/CMake/Dependencies.cmake
@@ -4,11 +4,35 @@ include(functions/dependency_options)
 
 dependency_options("SDL2" XM8_SYSTEM_SDL2 ON XM8_STATIC_SDL2)
 
-add_subdirectory(Builder/External/SDL2)
-if(TARGET SDL2::SDL2main)
-  set(SDL2_MAIN SDL2::SDL2main)
+if(XM8_SYSTEM_SDL2)
+  find_package(SDL2 REQUIRED)
+  if(TARGET SDL2::SDL2)
+    if(TARGET SDL2::SDL2main)
+      set(SDL2_MAIN SDL2::SDL2main)
+    endif()
+  elseif(TARGET SDL2::SDL2-static)
+    # On some distros, such as vitasdk, only the SDL2::SDL2-static target is available.
+    # Alias to SDL2::SDL2 because some finder scripts may refer to SDL2::SDL2.
+    add_library(SDL2::SDL2 ALIAS SDL2::SDL2-static)
+    if(TARGET SDL2::SDL2main)
+      set(SDL2_MAIN SDL2::SDL2main)
+    endif()
+  else()
+    # Assume an older Debian derivate that comes with an sdl2-config.cmake
+    # that only defines `SDL2_LIBRARIES` (as -lSDL2) and `SDL2_INCLUDE_DIRS`.
+    add_library(SDL2_lib INTERFACE)
+    target_link_libraries(SDL2_lib INTERFACE ${SDL2_LIBRARIES})
+    target_include_directories(SDL2_lib INTERFACE ${SDL2_INCLUDE_DIRS})
+    # Can't define an INTERFACE target with ::, so alias instead
+    add_library(SDL2::SDL2 ALIAS SDL2_lib)
+  endif()
+else()
+  add_subdirectory(Builder/External/SDL2)
+  if(TARGET SDL2::SDL2main)
+    set(SDL2_MAIN SDL2::SDL2main)
+  endif()
+  list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/Builder/External/SDL2/CMake")
 endif()
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/Builder/External/SDL2/CMake")
 
 add_library(XM8::SDL INTERFACE IMPORTED GLOBAL)
 if(TARGET SDL2::SDL2)
